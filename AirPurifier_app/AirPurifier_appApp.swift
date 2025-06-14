6f574edd5850ca2ca903ffbad7f19ab40d9ef500
Peepo <kanrayanapat@gmail.com>
2025-06-14 14:31:53 +0700

    Initial Commit

---
+++ AirPurifier_app/AirPurifier_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirPurifier_appApp.swift
+//  AirPurifier_app
+//
+//  Created by Kanlayanapat Thintupthai on 14/6/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct AirPurifier_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirPurifier_appTests/AirPurifier_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirPurifier_appTests.swift
+//  AirPurifier_appTests
+//
+//  Created by Kanlayanapat Thintupthai on 14/6/2568 BE.
+//
+
+import Testing
+@testable import AirPurifier_app
+
+struct AirPurifier_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
